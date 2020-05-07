e9537cf69b7e901e126991ffca5ecf5c80f441d9
Jose Guachun <joseguachuns@hotmail.com>
2020-05-07 17:37:10 -0500

    Creacion del header y footer con Botstrap

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+class user(models.Model):
+    email=models.EmailField(unique=True)
+    password=models.CharField(max_length=100)
+    first_name=models.CharField(max_length=100)
+    last_name=models.CharField(max_length=100)
+
+    is_Admin=models.BooleanField(default=False)
+
+    bio=models.TextField(blank=True)
+    birthdate=models.DateField(blank=True, null=True)
+    created=models.DateTimeField(auto_now_add=True)
+    modified=models.DateTimeField(auto_now=True)
+
 # Create your models here.
