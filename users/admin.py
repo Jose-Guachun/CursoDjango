54be75c66255b616aff9879fbaae266c366fabee
Jose Guachun <joseguachuns@hotmail.com>
2020-05-15 12:39:14 -0500

    dashboard de administracion

--- users/admin.py
+++ users/admin.py
@@ -1,15 +1,17 @@
 #clases de admin
 
 #Django
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib import admin
+from django.db import models
 
 #Models
+from django.contrib.auth.models import User
 from users.models import Profile
 
 # Register your models here.
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    
     list_display=('pk', 'user','phone_number', 'website', 'picture')
     list_display_links=('pk', 'user')
     list_editable=('phone_number', 'website', 'picture')
@@ -26,3 +28,39 @@ class ProfileAdmin(admin.ModelAdmin):
         'created',
         'modified',
     )
+
+    fieldsets=(
+        ('Profile', {
+            'fields':
+                ('user', 'picture',),
+        }),
+        ('Extra info',{
+            'fields':(
+                ('website','phone_number'),
+                ('biography'),
+            )
+        }),
+        ('Metadata', {
+            'fields':(('created', 'modified'),),
+        })
+    )
+    readonly_fields=('created', 'modified',)
+
+class ProfileInline(admin.StackedInline):
+    model=Profile
+    can_delete=False
+    verbose_name_plural='profiles'
+    
+class UserAdmin(BaseUserAdmin):
+    inlines=(ProfileInline,)
+    list_display=(
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_staff'
+    )
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin,)
